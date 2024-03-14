316093c5a44fe0d2cbaec62db4b2fbf9db37e2c8
Igor Kaseko <igorksk@gmail.com>
2024-03-14 10:06:51 +0200

    Create Login Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 
 import './App.css';
+import LoginForm from './LoginForm';
 
 function App() {
   return (
     <div className="App">
-          Learn React
+          <LoginForm />
     </div>
   );
 }
